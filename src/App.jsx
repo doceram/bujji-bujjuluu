d6a125f156d288501b39900a01bfddfcb964aad6
Doceram <rameshrudra178@gmail.com>
2025-12-31 14:45:15 +0530

    fix:updated jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Landing from "./components/Landing";
 import Journey from "./components/Journey";
-// import EnvelopeOpen from "./components/EnvelopeOpen";
 import EnvelopeReal from "./components/EnvelopeReal";
 import HeartsRain from "./components/HeartsRain";
 import Letter from "./components/Letter";
@@ -10,20 +9,24 @@ import SurpriseButton from "./components/SurpriseButton";
 function App() {
   const [step, setStep] = useState("landing");
 
+  useEffect(() => {
+    if (step === "hearts") {
+      const timer = setTimeout(() => {
+        setStep("letter");
+      }, 4000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [step]);
+
   return (
     <>
       {step === "landing" && <Landing onStart={() => setStep("journey")} />}
       {step === "journey" && <Journey onFinish={() => setStep("envelope")} />}
       {step === "envelope" && <EnvelopeReal onOpen={() => setStep("hearts")} />}
-      {step === "hearts" && (
-        <>
-          <HeartsRain />
-          {setTimeout(() => setStep("letter"), 4000)}
-        </>
-      )}
+      {step === "hearts" && <HeartsRain />}
       {step === "letter" && <Letter />}
 
-      {/* Music button always available */}
       <SurpriseButton />
     </>
   );
